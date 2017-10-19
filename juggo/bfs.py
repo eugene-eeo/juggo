fb0cea2d963935131a399de988ce40a59fee52d8
Eeo Jun <141bytes@gmail.com>
2017-10-19 10:07:55 +0100

    code cleanup

--- juggo/bfs.py
+++ juggo/bfs.py
@@ -5,20 +5,17 @@ from .utils import generate_ops, verify, vectors_eq
 def all_paths(initial, limits):
     ops = list(generate_ops(limits))
     q = deque([[initial]])
-    seen = set([initial])
+    seen = {initial}
     while q:
         path = q.popleft()
         u = path[-1]
         for op in ops:
             v = op(u)
-            if v == u: # no progress
-                continue
-            if verify(limits, v) and v not in seen:
+            if v not in seen and verify(limits, v):
                 seen.add(v)
                 p = path + [v]
                 q.append(p)
                 yield u, v, p
-    return []
 
 
 def search(initial, limits, target):
